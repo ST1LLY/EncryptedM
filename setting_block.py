db7791ba65192e71ad361f0e3b25cde46cc8e4e8
ST1LLY <savostconst@yandex.ru>
2017-06-09 15:19:22 +0300

    Create setting_block.py

--- setting_block.py
+++ setting_block.py
@@ -8,14 +8,14 @@ class setting_block(Frame):
 
     def sb_makewidgets(self,parent=None):
 
-        # Rows of frame for drawing widgets (Ряды фрейма для отрисовки виджетов)
+        # Rows of the frame for drawing widgets (Ряды фрейма для отрисовки виджетов)
         row_1 = Frame(parent)
         row_2 = Frame(parent)
 
         row_3 = Frame(parent)
         row_4 = Frame(parent)
 
-        # All set of widgets of setting block (Весь набор виджетов блока настроек)
+        # Whole set of widgets of the setting block (Весь набор виджетов блока настроек)
         lab_ip = Label(row_1, width=15,text='destination IP:')
         ent_ip = Entry(row_1)
 
@@ -27,25 +27,25 @@ class setting_block(Frame):
 
         btn = Button(row_4, text='start', command=self.sb_press_button)
 
-        # Drawing in 1st row of frame (Отрисовка на 1-м ряду фрейма)
+        # Drawing in 1st row of the frame (Отрисовка на 1-м ряду фрейма)
         row_1.pack(side=TOP, fill=X)
         lab_ip.pack(side=LEFT, fill=X)
         ent_ip.pack(side=RIGHT, expand=YES,fill=X)
         ent_ip.insert(0, '192.168.1.10')
 
-        # Drawing in 2nd row of frame (Отрисовка на 2-м ряду фрейма)
+        # Drawing in 2nd row of the frame (Отрисовка на 2-м ряду фрейма)
         row_2.pack(side=TOP, fill=X)
         lab_my_ip.pack(side=LEFT, fill=X)
         ent_my_ip.pack(side=RIGHT, expand=YES, fill=X)
         ent_my_ip.insert(0, '192.168.1.10')
 
-        # Drawing in 3rd row of frame (Отрисовка на 3-м ряду фрейма)
+        # Drawing in 3rd row of the frame (Отрисовка на 3-м ряду фрейма)
         row_3.pack(side=TOP, fill=X)
         lab_port.pack(side=LEFT, fill=X)
         ent_port.pack(side=RIGHT, expand=YES, fill=X)
         ent_port.insert(0,'50007')
 
-        # Drawing in 4th row of frame (Отрисовка на 4-м ряду фрейма)
+        # Drawing in 4th row of the frame (Отрисовка на 4-м ряду фрейма)
         row_4.pack(side=TOP, fill=X)
         btn.pack(side=RIGHT)
 
