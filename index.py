f0f4dc813cd65a3f967b08ab44328dc446ec1ab1
ST1LLY <savostconst@yandex.ru>
2017-06-09 15:43:27 +0300

    Create index.py

--- index.py
+++ index.py
@@ -20,7 +20,7 @@ class form(input_block, setting_block, output_block):
         self.pack()
         self.makewidgets()
 
-        self.AES_KEY_LENGTH = 16 #AES Key's bytes length to read from AES_KEY.txt
+        self.AES_KEY_LENGTH = 16 #AES Key's byte length stored in AES_KEY.txt (длина AES ключа в байтах, хранящегося в файле AES_KEY.txt)
 
         self.AES_KEY = open('AES_KEY.txt','rb').read(self.AES_KEY_LENGTH)
 
@@ -38,12 +38,12 @@ class form(input_block, setting_block, output_block):
     def now(self):
         return time.asctime()
 
-    #   Send message to destination host
+    #   Sending message to the remote host (Отправка сообщения на удаленный хост)
     def set_message(self,status=0,message=""):
 
         json_request = None
         connect_ip = self.sb_ent_ip.get()
-        serverPort = int(self.sb_ent_port.get())  # Get from form destination IP's port
+        serverPort = int(self.sb_ent_port.get())  # Retrieving the port number from the field (Получение из поля номера порта)
 
         if status == 0:
             json_request = json.dumps({
@@ -70,10 +70,10 @@ class form(input_block, setting_block, output_block):
             self.sockobj.close()
             return 1
         except:
-            self.set_connect_info("Error sending a message. Check out inputed data in setting block.")
+            self.set_connect_info("Error sending message. Check the data input in the setting block.")
             return 0
 
-    #   Reset interface
+    #   Resetting interface
     def reset_settings(self):
         self.sb_press = FALSE
         self.sb_btn.config(text='start')
@@ -81,7 +81,7 @@ class form(input_block, setting_block, output_block):
         self.sockobj_listen.close()
         self.set_connect_info('Connection disabled')
 
-    #   Open port and listen. Processing received messages.
+    #   Opening port and listening to it. Processing received messages. (Открытие порта и его прослушивание. Обработка полученных сообщений.)
     def able_connection(self,sockobj):
         flag = True
         while True:
@@ -107,7 +107,7 @@ class form(input_block, setting_block, output_block):
                 self.ob_settext('\nfrom: ' + self.now() + '\n' + decrypt_text.decode("utf-8"))
                 self.ob_text.see(END)
 
-    # "Send" button
+    # "Send" button (Кнопка "Send")
     def ib_press_button(self,event=None):
 
         message = self.ib_gettext().encode()
@@ -123,10 +123,10 @@ class form(input_block, setting_block, output_block):
         self.ob_settext(25*'-' + '\n' + text + '\n' + 25*'-')
         self.ob_text.see(END)
 
-    # "start/stop" button
+    # "start/stop" button (Кнопка "start/stop")
     def sb_press_button(self):
 
-        if not self.sb_press: # If clicked for "start" than initialize connection.
+        if not self.sb_press: # clicking the "start" button initializes connection. (Событие клика по кнопке "start" устанавливает соединение.)
             myHost = self.sb_ent_my_ip.get()
             myPort = int(self.sb_ent_port.get())
 
@@ -139,12 +139,12 @@ class form(input_block, setting_block, output_block):
                 self.set_connect_info("Error of opening port. Check out inputed data in setting block.")
                 return
 
-            self.set_connect_info('Socket '+ str(myHost)+":"+ str(myPort) + ' opened and listening') # Output data about processing
-            self.ib_btn.config(state=NORMAL) # Кнопка для отправки сообщений доступна. "Send" button is accessible
-            self.sb_btn.config(text='stop') # Кнопка в блоке настроек меняет свою функцию на сброс соединения
-            self.sb_press = TRUE # "Start" button pressed
+            self.set_connect_info('Socket '+ str(myHost)+":"+ str(myPort) + ' opened and listening')
+            self.ib_btn.config(state=NORMAL) 
+            self.sb_btn.config(text='stop') 
+            self.sb_press = TRUE
 
-        else: # If clicked "stop" than disable opened connection
+        else: # clicking the "stop" button initializes connection disables opened connection (Событие клика по кнопке "stop" - сброс открытого соединения.)
 
             self.set_message()
             self.reset_settings()
